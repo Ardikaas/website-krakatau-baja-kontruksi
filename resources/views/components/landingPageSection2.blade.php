490333bd081e4b116a3a2bf0ae7cc4867300d667
Mukhlis Gia Tegar <mukhgia12@gmail.com>
2025-12-27 14:39:10 +0700

    feature 1 done tinggal nambahin aset aset gambar

---
+++ resources/views/components/landingPageSection2.blade.php
@@ -0,0 +1,130 @@
+<section class="banner-carousel-section p_relative">
+    <!-- Carousel -->
+    <div class="banner-carousel owl-theme owl-carousel owl-dots-none">
+        <!-- Slide 1 -->
+        <div class="slide-item p_relative">
+            <div
+                class="pattern-layer-2"
+                style="background-image: url(assets/images/shape/shape-3.png);"
+            ></div>
+            <div
+                class="bg-layer"
+                style="background-image: url(assets/images/banner/banner-1.jpg);"
+            ></div>
+
+            <div class="outer-container clearfix">
+                <div class="content-box">
+                    <figure class="image-layer rotate-me">
+                        <img src="assets/images/resource/megnat-1.png" alt="" />
+                    </figure>
+
+                    <div class="inner-box">
+                        <h2>reliable <span>Metal <br />Services</span></h2>
+                        <p>
+                            Delivering high-quality, customized metal solutions
+                            with precision, efficiency, and advanced technology
+                            for diverse industrial applications.
+                        </p>
+                        <div class="btn-box">
+                            <a href="index.html" class="theme-btn btn-one">
+                                <i class="flaticon-right-arrow"></i>
+                                <span>Read More</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="slide-item p_relative">
+            <div
+                class="pattern-layer-2"
+                style="background-image: url(assets/images/shape/shape-3.png);"
+            ></div>
+            <div
+                class="bg-layer"
+                style="background-image: url(assets/images/banner/banner-2.jpg);"
+            ></div>
+
+            <div class="outer-container clearfix">
+                <div class="content-box">
+                    <figure class="image-layer rotate-me">
+                        <img src="assets/images/resource/megnat-1.png" alt="" />
+                    </figure>
+
+                    <div class="inner-box">
+                        <h2>Innovators <span>of the <br />Future.</span></h2>
+                        <p>
+                            Innovators of the future represents our commitment
+                            to pioneering cutting-edge solutions that drive
+                            progress and shape tomorrow's industry standards.
+                        </p>
+                        <div class="btn-box">
+                            <a href="index.html" class="theme-btn btn-one">
+                                <i class="flaticon-right-arrow"></i>
+                                <span>Read More</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="slide-item p_relative">
+            <div
+                class="pattern-layer-2"
+                style="background-image: url(assets/images/shape/shape-3.png);"
+            ></div>
+            <div
+                class="bg-layer"
+                style="background-image: url(assets/images/banner/banner-3.jpg);"
+            ></div>
+
+            <div class="outer-container clearfix">
+                <div class="content-box">
+                    <figure class="image-layer rotate-me">
+                        <img src="assets/images/resource/megnat-1.png" alt="" />
+                    </figure>
+
+                    <div class="inner-box">
+                        <h2>Top-Rated <br /><span>Solutions for You</span></h2>
+                        <p>
+                            Top-rated solutions designed to meet your unique
+                            needs, delivering innovation, quality, reliability,
+                            and performance for outstanding results every time.
+                        </p>
+                        <div class="btn-box">
+                            <a href="index.html" class="theme-btn btn-one">
+                                <i class="flaticon-right-arrow"></i>
+                                <span>Read More</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Right Side Content -->
+    <div class="banner-right-content">
+        <div class="inner-box">
+            <div class="mail-box">
+                <a href="mailto:getsupport@example.com">
+                    <i class="flaticon-share"></i>
+                    <span>getsupport@example.com</span>
+                </a>
+            </div>
+
+            <ul class="social-links">
+                <li><h5>On Socials</h5></li>
+                <li><a href="#"><i class="flaticon-facebook"></i></a></li>
+                <li><a href="#"><i class="flaticon-twitter"></i></a></li>
+                <li><a href="#"><i class="flaticon-instagram-logo"></i></a></li>
+                <li><a href="#"><i class="flaticon-linkedin"></i></a></li>
+                <li><a href="#"><i class="flaticon-youtube"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -4,6 +4,14 @@
 @section('meta_description', 'Official website of PT Krakatau Baja Konstruksi')
 
 @section('content')
-
-  <h1>Hello World</h1>
+  <div class="boxed-wrapper">
+    <div class="banner-section">
+      <div
+          class="pattern-layer"
+          style="background-image: url(assets/images/shape/shape-2.png);"
+      ></div>
+      <x-landingPageSection1 type="hero" title="Produsen|Baja |Berkualitas" />
+    </div>
+    <x-landingPageSection4,5 />
+  </div>
 @endsection
