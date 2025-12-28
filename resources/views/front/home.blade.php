c615ac4c2ea34617498ea5077bb5b9aeff69083d
Mukhlis Gia Tegar <mukhgia12@gmail.com>
2025-12-28 19:27:58 +0700

    section 2 selesai, nambah script dan ganti logo

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -4,14 +4,12 @@
 @section('meta_description', 'Official website of PT Krakatau Baja Konstruksi')
 
 @section('content')
-  <div class="boxed-wrapper">
-    <div class="banner-section">
-      <div
-          class="pattern-layer"
-          style="background-image: url(assets/images/shape/shape-2.png);"
-      ></div>
-      <x-landingPageSection1 type="hero" title="Produsen|Baja |Berkualitas" />
-    </div>
-    <x-landingPageSection4,5 />
+  <div class="banner-section">
+    <div
+        class="pattern-layer"
+    ></div>
+    <x-landingPageSection1 type="hero" title="Produsen|Baja |Berkualitas" />
+    <x-landingPageSection2 />
+    <x-landingPageSection3 />
   </div>
 @endsection
