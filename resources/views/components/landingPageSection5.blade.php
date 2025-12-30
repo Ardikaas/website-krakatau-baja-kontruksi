b59b8df5a72ea2fe2d3161760960631435532e05
Mukhlis Gia Tegar <mukhgia12@gmail.com>
2025-12-30 08:39:42 +0700

    nambahin section 10, ganti href di beberapa image di mobile menu, header dan menghapus folder yang duplikat

---
+++ resources/views/components/landingPageSection5.blade.php
@@ -0,0 +1,112 @@
+{{-- Service Section --}}
+<section class="service-section">
+    <div class="bg-layer" style="background-image: url({{ asset('images/background/service-bg.jpg') }});"></div>
+    <div class="auto-container">
+        <div class="inner-container">
+            {{-- Upper Box --}}
+            <div class="upper-box">
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 col-sm-12 count-column">
+                        <div class="count-box">
+                            <div class="shape" style="background-image: url({{ asset('images/shape/shape-5.png') }});"></div>
+                            <h2>50<span>k</span></h2>
+                            <h5>Units Produced Annually<br>with Precision.</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-md-12 col-sm-12 title-column">
+                        <div class="sec-title">
+                            <h6>Services</h6>
+                            <h2>Excellence in<br>Every <span>[Service]</span> Offered</h2>
+                            <p class="mt_12">Delivering quality, innovation, and precision.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            {{-- Lower Box --}}
+            <div class="lower-box">
+                {{-- Single Service Box --}}
+                <div class="single-service-box">
+                    <div class="inner-box">
+                        <figure class="image"><img src="{{ asset('images/resource/service-1.png') }}" alt="Service"></figure>
+                        <div class="lower-content">
+                            <h3>Discover Our Complete Service Range.</h3>
+                            <p>Services tailored to your needs.</p>
+                            <a href="#"><i class="flaticon-right-arrow"></i><span>All Services</span></a>
+                        </div>
+                    </div>
+                </div>
+                
+                {{-- Three Item Carousel --}}
+                <div class="three-item-carousel owl-carousel owl-theme owl-dots-none">
+                    {{-- Service Block 1 --}}
+                    <div class="service-block-one">
+                        <div class="inner-box">
+                            <div class="image-inner">
+                                <span class="count-text">01</span>
+                                <div class="image-box">
+                                    <div class="shape" style="background-image: url({{ asset('images/shape/shape-6.png') }});"></div>
+                                    <figure class="image"><img src="{{ asset('images/service/service-1.jpg') }}" alt="Service"></figure>
+                                    <div class="icon-box"><i class="flaticon-turning"></i></div>
+                                </div>
+                            </div>
+                            <div class="lower-content">
+                                <div class="block-title">
+                                    <div class="line-shape"></div>
+                                    <h3><a href="#">Fabrication</a></h3>
+                                </div>
+                                <p>Precision crafting metal structures with innovative techniques & expertise.</p>
+                                <div class="btn-box"><a href="#"><i class="flaticon-right-arrow"></i><span>Read More</span></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {{-- Service Block 2 --}}
+                    <div class="service-block-one">
+                        <div class="inner-box">
+                            <div class="image-inner">
+                                <span class="count-text">02</span>
+                                <div class="image-box">
+                                    <div class="shape" style="background-image: url({{ asset('images/shape/shape-6.png') }});"></div>
+                                    <figure class="image"><img src="{{ asset('images/service/service-1.jpg') }}" alt="Service"></figure>
+                                    <div class="icon-box"><i class="flaticon-bending"></i></div>
+                                </div>
+                            </div>
+                            <div class="lower-content">
+                                <div class="block-title">
+                                    <div class="line-shape"></div>
+                                    <h3><a href="#">Metal Processing</a></h3>
+                                </div>
+                                <p>Transforming raw materials into high quality metal products.</p>
+                                <div class="btn-box"><a href="#"><i class="flaticon-right-arrow"></i><span>Read More</span></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {{-- Service Block 3 --}}
+                    <div class="service-block-one">
+                        <div class="inner-box">
+                            <div class="image-inner">
+                                <span class="count-text">03</span>
+                                <div class="image-box">
+                                    <div class="shape" style="background-image: url({{ asset('images/shape/shape-6.png') }});"></div>
+                                    <figure class="image"><img src="{{ asset('images/service/service-1.jpg') }}" alt="Service"></figure>
+                                    <div class="icon-box"><i class="flaticon-beam"></i></div>
+                                </div>
+                            </div>
+                            <div class="lower-content">
+                                <div class="block-title">
+                                    <div class="line-shape"></div>
+                                    <h3><a href="#">Metal Casting</a></h3>
+                                </div>
+                                <p>Metal casting turns molten metal into solid shapes and products.</p>
+                                <div class="btn-box"><a href="#"><i class="flaticon-right-arrow"></i><span>Read More</span></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+{{-- Service Section End --}}
