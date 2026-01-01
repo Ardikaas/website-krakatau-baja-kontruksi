3c97ff278dbea74ba1019e79f64fac18d2207cde
penyulaut <ripanputra060904@gmail.com>
2026-01-01 21:21:22 +0700

    add sidebar and topbar admin

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\Front\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', function () {
+    return view('admin.testsidebarandtopbar');
+})->name('admin.dashboard');
