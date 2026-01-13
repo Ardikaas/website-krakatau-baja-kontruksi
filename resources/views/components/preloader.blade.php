aedf8c55c0d6abbd20f5b905983b1640bd489028
Barr <3337230089@untirta.ac.id>
2026-01-13 10:33:30 +0700

    nambah text PT KRAKATAU BAJA KONSTRUKSI  di loading

--- resources/views/components/preloader.blade.php
+++ resources/views/components/preloader.blade.php
@@ -5,30 +5,33 @@
                     <div class="animation-preloader">
                         <div class="spinner"></div>
                         <div class="txt-loading">
-                            <span data-text-preloader="k" class="letters-loading">
-                                k
-                            </span>
-                            <span data-text-preloader="r" class="letters-loading">
-                                r
-                            </span>
-                            <span data-text-preloader="a" class="letters-loading">
-                                a
-                            </span>
-                            <span data-text-preloader="k" class="letters-loading">
-                                k
-                            </span>
-                            <span data-text-preloader="a" class="letters-loading">
-                                a
-                            </span>
-                            <span data-text-preloader="t" class="letters-loading">
-                                t
-                            </span>
-                            <span data-text-preloader="a" class="letters-loading">
-                                a
-                            </span>
-                            <span data-text-preloader="u" class="letters-loading">
-                                u
-                            </span>
+                            <span data-text-preloader="P" class="letters-loading">P</span>
+                            <span data-text-preloader="T" class="letters-loading">T</span>
+                            <span class="letters-loading space">&nbsp;</span>
+                            <span data-text-preloader="K" class="letters-loading">K</span>
+                            <span data-text-preloader="r" class="letters-loading">r</span>
+                            <span data-text-preloader="a" class="letters-loading">a</span>
+                            <span data-text-preloader="k" class="letters-loading">k</span>
+                            <span data-text-preloader="a" class="letters-loading">a</span>
+                            <span data-text-preloader="t" class="letters-loading">t</span>
+                            <span data-text-preloader="a" class="letters-loading">a</span>
+                            <span data-text-preloader="u" class="letters-loading">u</span>
+                            <span class="letters-loading space">&nbsp;</span>
+                            <span data-text-preloader="B" class="letters-loading">B</span>
+                            <span data-text-preloader="a" class="letters-loading">a</span>
+                            <span data-text-preloader="j" class="letters-loading">j</span>
+                            <span data-text-preloader="a" class="letters-loading">a</span>
+                            <span class="letters-loading space">&nbsp;</span>
+                            <span data-text-preloader="K" class="letters-loading">K</span>
+                            <span data-text-preloader="o" class="letters-loading">o</span>
+                            <span data-text-preloader="n" class="letters-loading">n</span>
+                            <span data-text-preloader="s" class="letters-loading">s</span>
+                            <span data-text-preloader="t" class="letters-loading">t</span>
+                            <span data-text-preloader="r" class="letters-loading">r</span>
+                            <span data-text-preloader="u" class="letters-loading">u</span>
+                            <span data-text-preloader="k" class="letters-loading">k</span>
+                            <span data-text-preloader="s" class="letters-loading">s</span>
+                            <span data-text-preloader="i" class="letters-loading">i</span>
                         </div>
                     </div>  
                 </div>
