7804dfdf0ef2674d9293154f741b5ce62290383c
Ardikaas <ardikaajisetiawan@gmail.com>
2026-01-15 10:28:53 +0700

    fix: fixing some bug in development

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,6 +17,8 @@
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Zalando+Sans:ital,wght@0,200..900;1,200..900&display=swap"
+        rel="stylesheet">
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
